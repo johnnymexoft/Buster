689af195af212d4d390c6d469cc6e28eb883de5b
Joo <jools@mst.dk>
2019-07-30 14:56:52 +0200

    TRying to add console programme

---
+++ PrivateConsoleApp/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim c=0
+        Do While c < 12
+            c= c+1
+        Loop
+        Console.WriteLine("Here we go again ..... " & c & " Times")
+
+    End Sub
+
+End Module
